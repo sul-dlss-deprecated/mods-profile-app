31fae7e1b2835d6286736dbba132812427773002
Naomi Dushay <ndushay@stanford.edu>
2013-03-23 22:38:00 -0700

    helper method for arbitrary facet; lang index display shows values and counts

--- app/controllers/lang_controller.rb
+++ app/controllers/lang_controller.rb
@@ -5,11 +5,10 @@ class LangController < ApplicationController
   
   # get lang data from MODS profiler Solr index
   def index
-    solr_response = get_language_terms    
-    my_resp = Blacklight::SolrResponse.new(solr_response, {})
     solr_fname = 'language_languageTerm_sim'
+    solr_response = get_facet solr_fname
+    my_resp = Blacklight::SolrResponse.new(solr_response, {})
     @facet_field = my_resp.facets.select {|f| f.name == solr_fname}.first
-p @facet_field   
     @facet_field
   end
 end
--- lib/mods_profiler_app/solr_helper.rb
+++ lib/mods_profiler_app/solr_helper.rb
@@ -1,11 +1,12 @@
 # NAOMI_MUST_COMMENT_THIS_MODULE
 module ModsProfilerApp::SolrHelper
-  
-   # NAOMI_MUST_COMMENT_THIS_METHOD
-  def get_language_terms
+
+
+  # NAOMI_MUST_COMMENT_THIS_METHOD
+  def get_facet facet_fld_name
     path = blacklight_config.solr_path
     # want to pass in fq=collection:blah argument
-    params = {:rows => 0, 'facet.limit'.to_sym => -1, 'facet.field'.to_sym => 'language_languageTerm_sim'} 
+    params = {:rows => 0, 'facet.limit'.to_sym => -1, 'facet.field'.to_sym => facet_fld_name} 
     response = blacklight_solr.get(path, :params=> params)
     response
   rescue Errno::ECONNREFUSED => e

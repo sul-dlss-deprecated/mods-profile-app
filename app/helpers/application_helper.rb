144c30da39b707617952c21876468064348fe253
Naomi Dushay <ndushay@stanford.edu>
2013-03-26 15:25:52 -0700

    override should_render_facet? to NOT include it when there is only a missing value

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,7 @@ module ApplicationHelper
     "MODS profile experimental app"
   end
   
-  # override
+  # OVERRIDE to display missing facet values as '(absent)'
   # Standard display of a facet value in a list. Used in both _facets sidebar
   # partial and catalog/facet expanded list. Will output facet value name as
   # a link to add that to your restrictions, with count in parens. 
@@ -18,5 +18,15 @@ module ApplicationHelper
     " " + render_facet_count(item.hits)).html_safe
   end
 
-  
+  # OVERRIDE to NOT display if there are ONLY missing values
+  # Determine if Blacklight should render the display_facet or not
+  #
+  # By default, only render facets with items.
+  # @param [Blacklight::SolrResponse::Facets::FacetField] display_facet 
+  def should_render_facet? display_facet
+    # display when show is nil or true
+    display = facet_configuration_for_field(display_facet.name).show != false
+    return display && display_facet.items.present? && !(display_facet.items.size == 1 && !display_facet.items[0].value)
+  end
+
 end
